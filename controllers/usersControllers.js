8b4c43223e0461cd141794dc6f25d4b61fcf514d
Julien <maire94@gmail.com>
2025-08-14 12:14:05 +0200

    14-08

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,9 +3,25 @@ const User = require('../models/User')
 
 exports.showUser = async function (req, res) {
     try {
-        const anime = await User.findById(req.params.id)
-        res.status(200).json(anime)
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
     } catch (error) {
         throw error
     }    
+}
+
+exports.updateUser = async function (req, res) {
+    const prop = req.params.prop
+    const newName = req.body.name
+    try {
+        const user = await User.findOneAndUpdate({_id: req.params.id}, 
+                                                 {$set: {[prop]: newName}},
+                                                 {new: true}
+        )
+        console.log(user)
+        res.status(200).json(user)
+    } catch (error) {
+        throw error
+    }
+    
 }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('../controllers/usersControllers')
 
 router.get("/:id", controller.showUser)
+router.patch("/id/:id/prop/:prop", controller.updateUser)
 
 module.exports = router
